411795cb9649a6a8acc744076d89a06002d54626
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-10-12 17:47:20 +0300

    add correct_3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
+
+from catalog import views
 from catalog.apps import CatalogConfig
-
-from catalog.views import home, contacts
-
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", contacts, name='contacts'),
-    path("", home, name='home'),
+    path("", views.home, name='contacts'),
+    path("contacts/", views.home, name='home'),
 ]
