58aab09735cfb57eeecb078be62e41a92be259a7
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-10-12 13:10:37 +0300

    add views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home_data(request):
+    return render(request, 'app/home.html')
+
+def contacts_data(request):
+    return render(request, 'app/contacts.html')
+
+
+def show_home_data(request):
+    if request.method == 'GET':
+        return render(request, 'app/home.html')
+
+def contacts_send(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
+    return render(request, "catalog/contacts.html")
