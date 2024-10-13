e2533626e61ca61f9552fcdf3cf330445c0a6f0b
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-10-13 11:30:37 +0300

    adding correct_4

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", views.home, name='contacts'),
-    path("contacts/", views.home, name='home'),
+    path("", views.home, name='home'),
+    path("contacts/", views.contacts, name='contacts'),
 ]
