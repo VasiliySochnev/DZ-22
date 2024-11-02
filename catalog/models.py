8b14c066f3c3cc0d3dbd0c26e75e04f6c8fcbfdb
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-11-03 01:10:04 +0300

    adding correct for models

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='photos/', verbose_name='фотография')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='категория')
     purchase_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='цена за покупку')
-    created_at = models.DateTimeField(auto_now=True, verbose_name='дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
 
     def __str__(self):
