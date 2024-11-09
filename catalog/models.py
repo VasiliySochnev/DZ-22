89eb8742103c164a42c3449582ce3b0d36bfeea1
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-11-09 12:46:13 +0300

    adding product_info.html

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name_category = models.CharField(max_length=100)
-    descriptions_category = models.CharField(max_length=150)
+    descriptions_category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name_category
@@ -14,7 +14,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name_product = models.CharField(max_length=100, verbose_name='наименование')
-    description_product = models.CharField(max_length=150, verbose_name='описание')
+    description_product = models.CharField(max_length=100, verbose_name='описание')
     image = models.ImageField(upload_to='media/photos/', verbose_name='фотография')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='категория')
     purchase_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='цена за покупку')
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
-
 from django.http import HttpResponse
 
 
 def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
+
     return render(request, 'catalog/product_list.html', context)
 
-# def home(request):
-#     return render(request, 'catalog/home.html')
-#
 def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 def product_info(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    context = {'product': product,
-               }
+    context = {'product': product}
 
     return render(request, 'catalog/product_info.html', context)
 
