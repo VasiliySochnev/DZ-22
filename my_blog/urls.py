ca068826a8d80bf2019033912cc94e5ef4936d9f
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-11-11 11:11:11 +0300

    adding correct for README.md

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -9,8 +9,8 @@ app_name = MyBlogConfig.name
 urlpatterns = [
     path("my_blogs_list/", views.My_blogListView.as_view(), name='my_blogs_list'),
     path("my_blogs_detail/<int:pk>/", views.My_blogDetailView.as_view(), name='my_blogs_detail'),
-    path("my_blog_form/<int:pk>", views.My_blogCreateView.as_view(), name='my_blog_form'),
-    path("my_blog_update/<int:pk>", views.My_blogUpdateView.as_view(), name='my_blog_update'),
+    path("my_blog_form/<int:pk>/", views.My_blogCreateView.as_view(), name='my_blog_form'),
+    path("my_blog_update/<int:pk>/", views.My_blogUpdateView.as_view(), name='my_blog_update'),
     path("my_blogs_confirm_delete/<int:pk>", views.My_blogDeleteView.as_view(), name='my_blog_delete'),
 ]
 
