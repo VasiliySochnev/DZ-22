5c3113140c5aaa3589f4d837a7bcb03b8b5ec199
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-11-11 06:24:50 +0300

    adding views and urls for my_blog

--- my_blog/models.py
+++ my_blog/models.py
@@ -6,5 +6,12 @@ class My_blog(models.Model):
     content = models.TextField(max_length=500, verbose_name='содержимое')
     preview = models.ImageField(upload_to='media/images/', verbose_name='изображение')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
-    publication_sign = models.BooleanField
-    number_of_views = models.Count
+    publication_sign = models.BooleanField(verbose_name="Признак публикации", default=True)
+    number_of_views = models.PositiveIntegerField(verbose_name="колличество просмотров", default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
