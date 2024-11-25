e8f6cbf7479c2b1ce497e3725c688bbf754f97ca
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-11-25 15:26:54 +0300

    adding correct in model, view, urls and templates

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, max_length=100, help_text='email')
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', ]
